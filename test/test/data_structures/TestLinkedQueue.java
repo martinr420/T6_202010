95f40ba12eee8018e74352ac4372e4b4c3ae7e48
linamaria2006 <60269795+linamaria2006@users.noreply.github.com>
2020-02-12 22:58:47 -0500

    Actualizacion

--- test/test/data_structures/TestLinkedQueue.java
+++ test/test/data_structures/TestLinkedQueue.java
@@ -1,12 +1,16 @@
 package test.data_structures;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import junit.framework.TestCase;
 import model.data_structures.LinkedQueue;
 import model.data_structures.Nodo;
+import model.data_structures.noExisteObjetoException;
 
-public class TestLinkedQueue <K>
+public class TestLinkedQueue <K> extends TestCase
 
 {
 
@@ -30,11 +34,42 @@ public class TestLinkedQueue <K>
 	public void testEnqueue()
 	{
 		SetUp();
-	    linkedQueue.enqueue(new Nodo(12));
+	    linkedQueue.enqueue(new Nodo(1));
+	    assertEquals(8, linkedQueue.size());
 	}
 	
+	@Test
+	public void testDequeue() throws noExisteObjetoException
+	{
+		SetUp();
+		Nodo <K> nodo = linkedQueue.dequeue();
+		assertEquals (3, nodo.darGenerico());
+	}
 	
+	@Test
+	public void TestIsEmpty ()
+	{
+		SetUp();
+		boolean si = linkedQueue.isEmpty();
+		assertEquals(false, si);
+	}
 	
+	@Test
+	public void testSize ()
+	{
+		SetUp();
+		int tamaño = linkedQueue.size();
+		assertEquals(7, tamaño);
+	}
 	
+	@Test
+	public void testDarPrimero() throws noExisteObjetoException
+	{
+		SetUp();
+		Nodo <K> nodo = linkedQueue.darPrimero();
+		assertEquals (3, nodo.darGenerico());
+		
+		
+	}
 	
 }
--- test/test/data_structures/TestLinkedStack.java
+++ test/test/data_structures/TestLinkedStack.java
@@ -1,5 +1,77 @@
 package test.data_structures;
 
-public class TestLinkedStack {
+import org.junit.Before;
+import org.junit.Test;
 
+import junit.framework.TestCase;
+import model.data_structures.LinkedStack;
+import model.data_structures.Nodo;
+import model.data_structures.noExisteObjetoException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestLinkedStack<K> extends TestCase
+
+{
+
+	
+	private LinkedStack<K> linkedStack;
+	
+	
+	@Before 
+	private void SetUp()
+	{
+		linkedStack = new LinkedStack<K>();
+		
+		linkedStack.push(new Nodo (5));
+		linkedStack.push(new Nodo (30));
+		linkedStack.push(new Nodo (0));
+		linkedStack.push(new Nodo (1));
+		linkedStack.push(new Nodo (3));
+		linkedStack.push(new Nodo (12));
+		
+	}
+	
+	@Test
+	private void testPush()
+	{
+		SetUp();
+		linkedStack.push(new Nodo (4));
+		assertEquals(7, linkedStack.size());
+	}
+	
+	@Test 
+	private void testPop() throws noExisteObjetoException
+	{
+		SetUp();
+		Nodo<K> nodo = linkedStack.pop();
+		assertEquals(12, nodo.darGenerico());
+		
+	}
+	
+	@Test
+	private void testIsEmpty()
+	{
+		SetUp();
+		boolean si = linkedStack.isEmpty();
+		assertEquals(false , si);
+	}
+	
+	@Test
+	private void testSize()
+	{
+		SetUp();
+		int tamaño = linkedStack.size();
+		assertEquals(6, tamaño);
+	}
+	
+	@Test 
+	private void testDarUltimo() throws noExisteObjetoException
+	{
+		SetUp();
+		Nodo <K> ultimo = linkedStack.darUltimo();
+		assertEquals(12, ultimo.darGenerico());
+	}
+	
+	
 }

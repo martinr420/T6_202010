59ba3a10329700a41227465968739efe9bf0e424
linamaria2006 <60269795+linamaria2006@users.noreply.github.com>
2020-02-26 23:34:45 -0500

    Test

--- test/test/logic/TestModelo.java
+++ test/test/logic/TestModelo.java
@@ -1,51 +1,100 @@
 package test.logic;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import model.data_structures.ListaDoblementeEncadenada;
+import model.data_structures.Nodo;
 import model.logic.Modelo;
+import model.logic.Multa;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestModelo {
+import junit.framework.TestCase;
+
+public class TestModelo <K> extends TestCase
+{
 	
 	private Modelo modelo;
-	private static int CAPACIDAD=100;
 	
 	@Before
 	public void setUp1() {
+		
 		modelo= new Modelo();
 	}
 
-	public void setUp2() {
-	}
 
 	@Test
-	public void testModelo() {
-		assertTrue(modelo!=null);
-		assertEquals(0, modelo.darTamano());  // Modelo con 0 elementos presentes.
-	}
-
-	@Test
-	public void testDarTamano() {
-		// TODO
-	}
-
-	@Test
-	public void testAgregar() {
-		// TODO Completar la prueba
-	}
-
-	@Test
-	public void testBuscar() {
-		setUp2();
-		// TODO Completar la prueba
-	}
-
-	@Test
-	public void testEliminar() {
-		setUp2();
-		// TODO Completar la prueba
+	public void testShellSort()
+	{ 
+		setUp1();
+		
+		Integer datosAleatorios[] =  {4,5,0,1,2,3,43,5,4,1};
+		Integer datosAscendentes[] = {43,5,5,4,3,3,2,1,1,0};
+	    Integer datosDescendentes[] = {0,1,1,2,3,4,4,5,5,43};
+		
+	modelo.shellSort(datosAleatorios);
+	modelo.shellSort(datosAscendentes);
+	modelo.shellSort(datosDescendentes);
+ 
+		
+		Integer listaFinal[] = {0,1,1,2,3,4,4,5,5,43};
+		
+		assertTrue(Arrays.equals(datosAleatorios, listaFinal));
+		assertTrue(Arrays.equals(datosAscendentes,listaFinal));
+		assertTrue(Arrays.equals(datosDescendentes, listaFinal));
 		
 	}
-
+	
+	@Test
+	public void testMergeSort()
+	{ 
+		setUp1();
+		
+		Integer datosAleatorios[] =  {4,5,0,1,2,3,43,5,4,1};
+		Integer datosAscendentes[] = {43,5,5,4,3,3,2,1,1,0};
+	    Integer datosDescendentes[] = {0,1,1,2,3,4,4,5,5,43};
+		
+	modelo.mergeSort(datosAleatorios, 0, 9 );
+	modelo.mergeSort(datosAscendentes, 0, 9) ;
+	modelo.mergeSort(datosDescendentes, 0, 9);
+ 
+		
+		Integer listaFinal[] = {0,1,1,2,3,4,4,5,5,43};
+		
+		assertTrue(Arrays.equals(datosAleatorios, listaFinal));
+		assertTrue(Arrays.equals(datosAscendentes,listaFinal));
+		assertTrue(Arrays.equals(datosDescendentes, listaFinal));
+		
+	}
+	
+	@Test
+	public void testQuickSort()
+	{ 
+		setUp1();
+		
+		Integer datosAleatorios[] =  {4,5,0,1,2,3,43,5,4,1};
+		Integer datosAscendentes[] = {43,5,5,4,3,3,2,1,1,0};
+	    Integer datosDescendentes[] = {0,1,1,2,3,4,4,5,5,43};
+		
+	modelo.quickSort(datosAleatorios, 1, 10);
+	modelo.quickSort(datosAscendentes, 1, 10);
+	modelo.quickSort(datosDescendentes, 1, 10);
+ 
+		
+		Integer listaFinal[] = {0,1,1,2,3,4,4,5,5,43};
+		
+		assertTrue(Arrays.equals(datosAleatorios, listaFinal));
+		assertTrue(Arrays.equals(datosAscendentes,listaFinal));
+		assertTrue(Arrays.equals(datosDescendentes, listaFinal));
+		
+	}
+	
+	
+	
 }
+
+
